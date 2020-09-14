0e0968701a430ef07b4e9406e8b54c309cf1302d
blisn8405 <blisn8405@wrdsb.ca>
2020-09-14 16:15:28 +0000

    did question 4

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+   // create a Scanner for user input
+  Scanner input = new Scanner(System.in);
     
+    // initialize 2 points using 4 lines to find the slope
+
+    // declare a variable for x1
+    int x1 = 3;
+
+    // declare a variable for x2
+    int x2= 8;
+
+    // declare a variable for y1
+    int y1 = 6;
+
+    // declare a variable for y2
+    int y2 = 16;
+
+    // declare and initialize the slope of the line
+     System.out.println("The slope is " + (y2 -y1)/(x2 - x1));
   }
 }
